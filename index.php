670dfbfda078f087334bf8a3e123d42d3c122d47
notu2 <nakindo47@gmail.com>
2022-02-03 21:15:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos maiores eius rerum fugit consectetur eveniet velit nostrum nemo quae soluta eligendi, in asperiores ipsa, ea officiis iste animi debitis quod.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
